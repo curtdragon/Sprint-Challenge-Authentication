6bdbf8d1a55c73fd9cc0b9016679832703925f3e
curtdragon <tealturboser@gmail.com>
2019-09-16 18:18:42 -0500

    Register and log in complete

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,25 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({
+          you: 'shall not pass!'
+        });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({
+      message: "No token Provided"
+    })
+  }
 };
